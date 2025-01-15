699eb8acc84b42f5ef1bc97cb6758403dcb03bbd
cristian osorio <cris300oso@gmail.com>
2025-01-15 20:39:03 +0100

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -20,19 +20,52 @@ MAIN_MENU(){
   *) MAIN_MENU "I could not find that service. What would you like today?" ;;
   esac
 }
-CUT(){
-}
-COLOR(){}
-PERM(){}
-STYLE(){}
-TRIM(){}
 FETCH_CUSTOMER_ID(){
   echo -e "\nWhat's your phone number?"
   read PHONE_NUMBER
   #fetch customer by phone number
   CUSTOMER_ID=$($PSQL "select customer_id from customers where phone='$PHONE_NUMBER'")
   #if customer does not exist in the database
-  if [[]]
-  then
+  if [[ -z $CUSTOMER_ID ]]
+    then
+    #get customer name
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read NAME
+    #insert customer in the database
+    CUSTOMER_INSERT_RESULT=$($PSQL "insert into customers(name,phone) values('$NAME','$PHONE_NUMBER')" 2>&1)
+    #validate if had an error during the insert
+    if [[ $? -ne 0]]
+      then
+      #there was an error, redirect to main menu
+       MAIN_MENU "There was an error during the creation of the customer, please try again."
+    fi
+    #get customer's id 
+    CUSTOMER_ID=$($PSQL "select customer_id from customers where phone='$PHONE_NUMBER'")
   fi
+  #return customer's id
+  echo $CUSTOMER_ID
+}
+CUT(){
+   CUSTOMER_ID=$(FETCH_CUSTOMER_ID)
+  if [[ -n $CUSTOMER_ID ]]
+  then
+   #create the service
+
+  fi
+}
+COLOR(){
+  CUSTOMER_ID=$(FETCH_CUSTOMER_ID)
+  if [[ -z $CUSTOMER_ID ]]
+  then
+   #return to main menu
+  fi
+}
+PERM(){
+  CUSTOMER_ID=$(FETCH_CUSTOMER_ID)
+}
+STYLE(){
+  CUSTOMER_ID=$(FETCH_CUSTOMER_ID)
+}
+TRIM(){
+  CUSTOMER_ID=$(FETCH_CUSTOMER_ID)
 }
