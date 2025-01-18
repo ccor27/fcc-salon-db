a2017dda6c19d6f69652cdca13ee03842632e583
cristian osorio <cris300oso@gmail.com>
2025-01-18 17:08:27 +0100

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PSQL=("psql -X --username=freecodecamp --dbname=salon --tuples-only -c")
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
-echo -e "\nWelcome to My Salon, how can I help you?\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
 
 MAIN_MENU(){
 if [[ $1 ]]
@@ -13,12 +13,12 @@ SERVICES=$($PSQL "select service_id,name from services")
 #show services propertly
 echo "$SERVICES" | while read SERVICE_ID BAR NAME
 do
- echo "$SERVICE_ID) $NAME"
+ echo "$SERVICE_ID) $NAME Service"
 done
 #get selected service's id
-read SERVICE_SELECTED
-SEVICE_SELECTED_ID=$($PSQL "select service_id from services where service_id=$SERVICE_SELECTED")
-if [[ -z $SEVICE_SELECTED_ID ]]
+read SERVICE_ID_SELECTED
+SERVICE_SELECTED_ID=$($PSQL "select service_id from services where service_id=$SERVICE_ID_SELECTED")
+if [[ -z $SERVICE_SELECTED_ID ]]
 then
  MAIN_MENU "I could not find that service. What would you like today?"
 fi
@@ -37,16 +37,16 @@ read CUSTOMER_NAME
 #insert customer into database
 CUSTOMER_INSERT_RESULT=$($PSQL "insert into customers(name,phone) values('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
 fi
-CUSTOMER_ID=$($PSQL "select customer_id from customers where name='$CUSTOMER_NAME'")
-SERVICE_FORMATED_NAME=$($PSQL "select name from services where service_id='$SEVICE_SELECTED_ID'" | sed -r 's/^ *| *$//g')
+CUSTOMER_ID=$($PSQL "select customer_id from customers where phone='$CUSTOMER_PHONE'")
+SERVICE_FORMATED_NAME=$($PSQL "select name from services where service_id='$SERVICE_SELECTED_ID'" | sed -r 's/^ *| *$//g')
 CUSTOMER_FORMATED_NAME=$(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')
 #ask for time
 echo -e "\nWhat time would you like your $SERVICE_FORMATED_NAME, $CUSTOMER_FORMATED_NAME?"
 #get time
-read TIME
-TIME_FORMATED=$(echo $TIME | | sed -r 's/^ *| *$//g')
+read SERVICE_TIME
+TIME_FORMATED=$(echo $SERVICE_TIME | sed -r 's/^ *| *$//g')
 #insert into appointment
-APPOINTMENT_INSERT_RESULT=$($PSQL "insert into appoinments(customer_id,service_id,time) values($CUSTOMER_ID,$SERVICE_SELECTED_ID,'$TIME')")
+APPOINTMENT_INSERT_RESULT=$($PSQL "insert into appointments(customer_id,service_id,time) values($CUSTOMER_ID,$SERVICE_SELECTED_ID,'$TIME')")
 #show final message
 echo -e "\nI have put you down for a $SERVICE_FORMATED_NAME at $TIME_FORMATED, $CUSTOMER_FORMATED_NAME."
 #show menu again (list of serices)
